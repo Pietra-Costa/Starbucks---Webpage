036f9a679a698860b7afdc03b4d29823892e9337
Pietra Costa <pietracosta.dev@gmail.com>
2024-07-15 17:15:26 -0300

    Add files via upload

---
+++ starbucks/script.js
@@ -0,0 +1,9 @@
+function imgSlider(nd){
+    document.querySelector(".starbucks").src = nd;
+
+}
+
+function circleColor(cc){
+    const circle = document.querySelector('.circle');
+    circle.style.background = cc;
+}
